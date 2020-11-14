94b937577f3ce9c54c9ac454b77ce1cb939fbd01
Kat21 <49850194+datkat21@users.noreply.github.com>
2020-11-13 18:17:25 -0600

    Add Minishell 1.1
    Test
    
    Co-Authored-By: Exeon <46092609+actuallyexeon@users.noreply.github.com>

--- mini.sh
+++ mini.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #Setup the functions
-toilet "MiniShell" -f mini --metal
-echo -e "\e[1;34mMiniShell \e[1;35m1.0 \e[0;0mby CoconutDev. Type \e[1;36mhelp \e[0;0mfor commands."
 function jumpto() {
     label=$1
     cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
@@ -9,30 +7,78 @@ function jumpto() {
     exit
 }
 
-start=${1:-"start"}
-echo "Checking for packages.."
-command -v toilet
-if [ "$?" == "1" ]; then
-    echo "Toilet not found. Please install it!"
-fi
-command -v figlet
-if [ "$?" == "1" ]; then
-    echo "Figlet not found. Please install it!"
-fi
+spinner() {
+    local i sp n
+    sp='/-\|'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\b" "${sp:i++%n:1}"
+    done
+}
 
-jumpto $start
+start=${1:-"start"}
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo -e "\e[1;34m[Info] \e[0;0mChecking for packages.."
+    command -v dialog >>/dev/null
+    if [ "$?" == "1" ]; then
+        echo -e "\e[1;37m[Warning] \e[0;0mDialog not found. Please install it!"
+    else
+        successRate=$(($successRate + 1))
+    fi
+    command -v figlet >>/dev/null
+    if [ "$?" == "1" ]; then
+        echo -e "\e[1;37m[Warning] \e[0;0mFiglet not found. Please install it!"
+    else
+        successRate=$(($successRate + 1))
+    fi
+    command -v toilet >>/dev/null
+    if [ $? == "1" ]; then # exeon takes a bath 5:10 PM 11/13/20 also i had idea to make the echo comamnd the draw command like draw the logo at startup i tried that idk
+        echo -e "\e[1;37m[Warning] \e[0;0mToilet not found. Please install it!"
+    else
+        successRate=$(($successRate + 1))
+        toilet "MiniShell" -f mini --metal
+    fi
+    packagewarning="\e[0;31mPlease refer to this link for missing packages:\nhttps://github.com/coconutteamdev/minishell/tree/v1.0#troubleshooting"
+    if [ "$successRate" == "3" ]; then
+        echo -e "\e[1;34m[Info] \e[0;0mPackage checking success."
+    elif [ "$successRate" == "1" ]; then
+        echo -e "$packagewarning"
+        echo -e "\e[1;37m[Warning] \e[0;0mMissing 2 packages. Please check above."
+    elif [ "$successRate" == "2" ]; then
+        echo -e "$packagewarning"
+        echo -e "\e[1;37m[Warning] \e[0;0mMissing 1 package. Please check above."
+    elif [ "$successRate" == "" ]; then
+        echo -e "$packagewarning"
+        echo -e "\e[1;31m[Error] \e[0;0mMissing all required packages. Please check above."
+    fi
+    echo -e "\e[1;36mMiniShell v1.1 \e[0;0mby CoconutDev. Type \e[1;36mhelp \e[0;0mfor commands."
+    jumpto $start
+else
+    echo -e "\e[1;31m[Error] \e[0;0mPlease do not run MiniShell as root or sudo."
+    exit
+fi
 
 start:
-printf "mini> "
-# Ask the user for their name
-# printf "$ "
+# Show the MiniShell prompt
+echo -n -e "\e[1;34mminishell>\e[0;0m "
 IFS=$'\t'
-array=("help\techo\texit\tmsgbox\tcls\twelcome")
+array=("help\tdraw\texit\tmsgbox\tcls\twelcome\treset\tgh")
 unset IFS
 read value
 if [[ "\t${array[@]}\t" =~ "\t${value}\t" ]]; then
     if [ "$value" == "help" ]; then
-        echo -e "help        Show this menu\nwelcome     Show the start screen\nexit        Quit the program\necho        Print a value\nmsgbox      Show a dialog box\ncls         Clear screen"
+        echo -e "help            Show this menu\nwelcome         Show the start screen\nexit            Quit the program\necho   [value]  Print a value\nmsgbox [value]  Show a dialog box\ncls             Clear screen"
+        jumpto start
+    elif [ "$value" == "gh" ]; then
+        printf '\nOpening.. '
+        spinner &
+
+        xdg-open https://github.com/coconutteamdev/minishell
+
+        kill "$!" # kill the spinner
+        printf '\n'
         jumpto start
     elif [ "$value" == "exit" ]; then
         echo "Exiting"
@@ -43,8 +89,15 @@ if [[ "\t${array[@]}\t" =~ "\t${value}\t" ]]; then
     elif [ "$value" == "welcome" ]; then
         clear
         jumpto welcome
+    elif [ "$value" == "reset" ]; then
+        clear
+        $(basename $0) && clear && exit
+    elif [ "$value" == "github" ]; then
+        echo "Opening the GitHub repo..."
+        xdg-open https://www.github.com/coconutteamdev/minishell
+
     fi
-elif [[ "$value" == *"echo "* ]]; then
+elif [[ "$value" == *"draw "* ]]; then
     echo $value >>file.tmp
     cut -c 6- file.tmp
     rm file.tmp
@@ -58,11 +111,27 @@ elif [[ "$value" == *"msgbox "* ]]; then
     rm file2.tmp
     jumpto start
 else
-    echo $value: invalid command
-    jumpto start
+    if [[ "$value" == *"set "* ]]; then
+        echo -e "\e[1;31m[Error] \e[0;0mThe set command is unfinished. Try another."
+        jumpto start
+    elif [ "$value" == *"exit"* ]; then
+        echo "$value: invalid command --did you mean exit?"
+        jumpto start
+    elif [ "$value" == *"welcome"* ]; then
+        echo "$value: invalid command --did you mean welcome?"
+        jumpto start
+    elif [ "$value" == *"cls"* ]; then
+        echo "$value: invalid command --did you mean cls?"
+        jumpto start
+    elif [ "$value" == "" ]; then
+        jumpto start
+    else
+        echo $value: invalid command
+        jumpto start
+    fi
 fi
 
 welcome:
 toilet "MiniShell" -f mini --metal
-echo -e "\e[1;34mMiniShell \e[1;35m1.0 \e[0;0mby CoconutDev. Type \e[1;36mhelp \e[0;0mfor commands."
+echo -e "\e[1;34mMiniShell \e[1;35mv1.1 \e[0;0mby CoconutDev. Type \e[1;36mhelp \e[0;0mfor commands."
 jumpto start
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
-# ![MiniShell logo](./logo.svg)
+# ![MiniShell](./logo.svg)
 
 ![GitHub last commit](https://img.shields.io/github/last-commit/datkat21/minishell?style=flat-square)
 
-![GitHub Release Date](https://img.shields.io/github/release-date/datkat21/minishell?style=flat-square)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/datkat21/minishell?style=flat-square)](https://github.com/coconutteamdev/minishell/releases)
 
 MiniShell is a small "shell" made in Bash for fun.
 You can type `help` for some commands.
@@ -11,16 +11,62 @@ You can type `help` for some commands.
 
 Clone the repository, you need `git` .
 
-```git clone https://github.com/datkat21/minishell```
+``` 
+git clone https://github.com/datkat21/minishell
+```
 
 Next, run the script.
 
-```bash minishell/mini.sh```
+``` 
+bash minishell/mini.sh
+```
 
 ## Troubleshooting
 
-`toilet` and `figlet` are required to display the welcome splash.
+(Note that `toilet` , `figlet` , and `dialog` are required for MiniShell to work.)
 
-![Discord](https://img.shields.io/discord/507735969731182592?style=flat-square)  
+### Missing all packages error
 
-![GitHub release (latest by date)](https://img.shields.io/github/v/release/datkat21/minishell?style=flat-square)
+If you get this during startup, 
+
+```[Warning] Dialog not found. Please install it!
+[Warning] Figlet not found. Please install it!
+[Warning] Toilet not found. Please install it!
+[Error] Missing all required packages. Please check above.
+
+``` 
+
+please make sure you have `toilet` , `figlet` , and `dialog` installed.
+
+On any distro that has the `apt` command, you can use this handy command to install them:
+
+```
+
+sudo apt install toilet figlet dialog
+
+``` 
+
+### Missing X packages error
+
+If you get this error at startup:
+
+```
+
+[Warning] Missing 2 packages. Please check above.
+
+``` 
+
+check the text above and install those packages. If it says, for example, 
+
+```
+
+[Warning] Dialog not found. Please install it!
+[Warning] Toilet not found. Please install it!
+[Warning] Missing 2 packages. Please check above.
+```
+
+then you would install those packages using ex. `sudo apt install toilet dialog`
+
+# Support
+
+[![Discord](https://img.shields.io/discord/507735969731182592?style=flat-square)](https://discord.gg/KgQGXEGHDE)
